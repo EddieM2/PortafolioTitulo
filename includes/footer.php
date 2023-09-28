2eae974aa99c958bfa8b5c21b5ad95eb2e21a378
chris <chrisq542@gmail.com>
2023-09-27 22:34:34 -0300

    header y footer

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <link rel="stylesheet" href="src/css/footer.css">
+</head>
+<body>
+
+
+ 
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    
+                    <a href="https://www.facebook.com" target="_blank">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    
+                    <a href="https://www.twitter.com" target="_blank">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    
+                    <a href="https://mail.google.com" target="_blank">
+                        <i class="far fa-envelope"></i>
+                    </a>
+                </div>
+                <div class="col-md-6 text-md-end">
+                    &copy; 2023 Tu Compañía. Todos los derechos reservados.
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
