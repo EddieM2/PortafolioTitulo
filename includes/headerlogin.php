c9d4f2007e73a36b5ab753c925d35ebf174baa9f
chris <chrisq542@gmail.com>
2023-09-30 10:11:58 -0300

    css login

---
+++ includes/headerlogin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tu Título</title>
+    <link rel="stylesheet" href="src/css/headerlogin.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="container">
+            <ul class="nav-links">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+            <button class="header-button"><a href="./index.php" class="logo">Inicio</a></button>
+        </div>
+    </nav>
+</body>
+</html>
